073954a04850bbcb94b7364966e25b45201e521a
DONGUK777 <kimdongyork@gmail.com>
2024-07-26 16:29:34 +0900

    테스트 추가

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,7 +1,29 @@
+import requests
+import os
+
 def req(dt="20120101"):
+    # url = gen_url('20240720')
+    url = gen_url(dt)
+    r = requests.get(url)
+    code = r.status_code
+    # return r.status_code
+    data = r.json()    
+    print(data)
+    return code, data
+    
+def gen_url(dt="20120101"):
     base_url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-    key = "84c02b6b6453e15a1a3ac31d2a1b59ab"
+    key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
-    print(url)
 
+    return url
 
+def get_key():
+    key = os.getenv('MOVIE_API_KEY')
+    return key
+
+def req2dataframe():
+    _, data = req()
+    # data.get('').get('')
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
