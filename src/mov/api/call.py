175a1e42fbaed5116f5b7eda6071b684a2a99de8
DONGUK777 <kimdongyork@gmail.com>
2024-07-29 15:37:42 +0900

    edit

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -50,7 +50,7 @@ def save2df(load_dt='20120101'):
     df = list2df(load_dt)
     # df에 load_dt 컴럼 추가 (조회 일자 YYMMDD 형식으로)
     # 아래 파일 저장시 load_dt 기본으로 파티셔닝
-    df['load_dt'] = '20120101'
+    df['load_dt'] = load_dt
     print(df.head(5))
     df.to_parquet('<PATH>', partition_cols=['load_dt'])
     return df
